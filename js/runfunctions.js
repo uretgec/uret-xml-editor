b15897128afa6590f0e72251d1986b690f5f8e7c
Tuna <heytbecom@gmail.com>
2012-11-19 11:37:14 +0200

    Update js/runfunctions.js
    alert if filename didnt exist

--- js/runfunctions.js
+++ js/runfunctions.js
@@ -4,7 +4,14 @@ $(document).ready(function(){
 	var uconfig = $("#config");
 	var tag_type = $("#tag_type");
 	var add_new = $("#add_new a");
-	
+	var filename = $("#file_name").val();
+
+	$("#config_form").live('click',function(){
+		if(filename === ""){
+			alert("Dosya ismini yazmayı unuttun.");
+			return false;
+		}
+	});
 	uadd.live('click',function(){
 		var config_count = $(".config").length;
 		uconfig.append('<li class="config" id="u'+config_count+'"><p><label for="title_name">Title</label>:<input name="u'+config_count+'[title_name]" id="title_name"><span class="desc">Write input label title</span></p><p><label for="tag_name">Tag Name</label>:<input name="u'+config_count+'[tag_name]" id="tag_name"><span class="desc">Write xml element name</span></p><p><label for="tag_desc">Tag Desc</label>:<input name="u'+config_count+'[tag_desc]" id="tag_desc"><span class="desc">Write this option description</span></p><p><label for="tag_type">Tag Type</label>:<select name="u'+config_count+'[tag_type]" id="tag_type"><option value="text">Text Box</option><option value="textarea">Textarea</option><option value="selectbox">Selectbox</option><option value="radio">Radio Box</option><option value="checkbox">Check Box</option><option value="image">Image Box</option><option value="multitext">Multi Text Box</option><option value="multitextarea">Multi Textarea</option><option value="multiselectbox">Multi Selectbox</option><option value="multiradio">Multi Radio Box</option><option value="multicheckbox">Multi Check Box</option><option value="multiimage">Multi Image Box</option></select><span class="desc">Select input box\'s type</span></p><p id="wed" style="display:none;"><label for="type_value">Tag Values</label>:<input name="u'+config_count+'[type_value]" id="type_value"><span class="desc">Write custom values (Not ready)</span></p><a href="#" id="delete">Delete</a></li>');
